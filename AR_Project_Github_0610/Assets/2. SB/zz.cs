2c1087b63bae890f80d3c1b6cbd51ea8cc316d26
HyunMin Lee <hmlee4135@naver.com>
2022-06-11 16:23:04 +0900

    AR 맵 띄우기

--- AR_Project_Github_0610/Assets/2. SB/zz.cs
+++ AR_Project_Github_0610/Assets/2. SB/zz.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class zz : MonoBehaviour
+public class aaaaa : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
